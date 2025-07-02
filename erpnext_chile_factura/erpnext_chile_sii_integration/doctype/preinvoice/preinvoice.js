a70f4b4df72d17f409dd520c029cc2d8f878ee09
Antonio Canada Momblant <toni.cm@gmail.com>
2025-07-02 19:27:44 +0000

    Fuerza campos de Preinvoice como read-only

--- erpnext_chile_factura/erpnext_chile_sii_integration/doctype/preinvoice/preinvoice.js
+++ erpnext_chile_factura/erpnext_chile_sii_integration/doctype/preinvoice/preinvoice.js
@@ -1,38 +1,51 @@
 frappe.ui.form.on('PreInvoice', {
-	onload: function (frm) {
-		frm.set_read_only();  // Todo el formulario read-only
-		frm.disable_save();   // Oculta botón "Guardar"
+  onload: function (frm) {
+    make_preinvoice_read_only(frm);
+  },
+  refresh: function (frm) {
+    make_preinvoice_read_only(frm);
 
-		// Asegurarse de que todas las tablas hijas también sean solo lectura
-		const readOnlyTables = [
-			"items",
-			"referencias",
-			"emisor_detalle",
-			"transporte",
-			"descuentos_recargos",
-			"otros_impuestos"
-		];
-
-		readOnlyTables.forEach(fieldname => {
-			frm.set_df(fieldname, {
-				read_only: 1
-			});
-		});
-	},
-	refresh: function (frm) {
-		if (frm.doc.estado === "Confirmada" && !frm.doc.pinv_creada) {
-			frm.add_custom_button("Crear PINV desde reglas", function () {
-				frappe.call({
-					method: "erpnext_chile_factura.erpnext_chile_sii_integration.doctype.preinvoice.preinvoice.make_purchase_invoice_from_rules",
-					args: { preinvoice_name: frm.doc.name },
-					callback: function (r) {
-						if (!r.exc) {
-							frappe.msgprint(r.message);
-							frm.reload_doc();
-						}
-					}
-				});
-			}, __("Acciones"));
-		}
-	}
+    // Agregar botón solo si corresponde
+    if (frm.doc.estado === "Confirmada" && !frm.doc.pinv_creada) {
+      frm.add_custom_button("Crear PINV desde reglas", function () {
+        frappe.call({
+          method: "erpnext_chile_factura.erpnext_chile_sii_integration.doctype.preinvoice.preinvoice.make_purchase_invoice_from_rules",
+          args: { preinvoice_name: frm.doc.name },
+          callback: function (r) {
+            if (!r.exc) {
+              frappe.msgprint(r.message);
+              frm.reload_doc();
+            }
+          }
+        });
+      }, __("Acciones"));
+    }
+  }
 });
+
+function make_preinvoice_read_only(frm) {
+  frm.set_read_only();
+  frm.disable_save();
+
+  const readOnlyTables = [
+    "items",
+    "referencias",
+    "emisor_detalle",
+    "transporte",
+    "descuentos_recargos",
+    "otros_impuestos"
+  ];
+
+  readOnlyTables.forEach(fieldname => {
+    frm.set_df(fieldname, { read_only: 1 });
+
+    const grid = frm.fields_dict[fieldname]?.grid;
+    if (grid) {
+      grid.can_add_rows = false;
+      grid.wrapper.find('.grid-add-row').hide();
+      grid.wrapper.find('.grid-remove-rows').hide();
+      grid.wrapper.find('.grid-buttons').hide();
+      grid.refresh();
+    }
+  });
+}
